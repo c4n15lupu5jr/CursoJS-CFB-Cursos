673e4c768cced6c854acbbb65af4ee7575950c5a
c4n15lupu5jr <84355075+c4n15lupu5jr@users.noreply.github.com>
2023-02-02 08:39:22 -0300

    aula 04, 05, 06 criados

---
+++ aula04.js
@@ -0,0 +1,9 @@
+let num1=0,num2=0,res=0
+
+num1=10
+num2=10
+
+res=(num+num2)*2
+
+console.log(res)
+
---
+++ aula06.js
@@ -0,0 +1,11 @@
+let n1,n2,n3,n4
+n1=10
+n2=5
+n3=15
+n4=2
+
+if(n1>n2) {
+    console.log(n1 + " maior que " + n2)
+} else {
+    console.log(n1 + " menor que " + n2)
+}
